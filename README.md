2a4d644bccd02bea4979dfe082ddafb2add36a6c
Matthew Parkinson <matthewparkinson44@yahoo.com>
2025-05-08 15:19:30 -0500

    Add auth module and README

---
+++ README.md
@@ -0,0 +1,1 @@
+"# River City Time Clock" 
---
+++ auth.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from models import db, User
+
+auth_bp = Blueprint('auth', __name__)
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@auth_bp.route('/login', methods=['GET','POST'])
+def login():
+    if request.method=='POST':
+        u = User.query.filter_by(username=request.form['username']).first()
+        if u and u.check_password(request.form['password']):
+            login_user(u)
+            return redirect(url_for('index'))
+        flash('Invalid credentials','danger')
+    return render_template('login.html')
+
+@auth_bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
